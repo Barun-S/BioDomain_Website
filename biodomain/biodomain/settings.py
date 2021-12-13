15776ade8bca9594f04652ae1ad4f0ae360d0801
Barun-S <66858557+Barun-S@users.noreply.github.com>
2021-12-13 12:39:42 +0530

    queries
    IITJ CASE data

--- biodomain/biodomain/settings.py
+++ biodomain/biodomain/settings.py
@@ -137,6 +137,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'contactbioequipzone@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_HOST_USER = 'contactbioequipzone@gmail.com' 
-EMAIL_HOST_PASSWORD = '<password>' 
+EMAIL_HOST_PASSWORD = 'bio0equipz0n@iitj' 
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True 
